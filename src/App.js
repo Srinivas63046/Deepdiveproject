5de46b0d0b5f867a00d3c0688a3803c24ab1d7b7
srinivas <Srinivas63046@gmail.com>
2023-09-14 17:34:08 +0530

    Deepdive-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+
+import React from "react";
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './Component09/Layout';
+import Pets from './Component09/Pets';
+import Services from './Component09/Services';
+import Home from './Component09/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route path="Pets" element={<Pets />} />
+          <Route path="Services" element={<Services />} />
+          <Route path="Home" element={<Home />} />
+          
+        </Routes>
+      </BrowserRouter>
+
+      
     </div>
   );
 }
